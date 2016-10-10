d25ae362748c0c1a92b50487fabf578c52ad81fd
PrivateSniper <privatesniper@gmail.com>
2016-10-10 16:23:07 +0100

    Add data to home views.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $data = $this->getData();
+        return view('home', $data);
     }
 }
