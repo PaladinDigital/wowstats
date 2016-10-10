6f1df73a17ec817b490223438a16a23a69875a18
PrivateSniper <privatesniper@gmail.com>
2016-10-10 18:46:37 +0100

    Set horde as default theme.

--- config/wow.php
+++ config/wow.php
@@ -4,7 +4,7 @@ return [
     'app'   => [
         'name' => 'Stats Tracker',
         // Valid Themes: 'alliance', 'horde', 'none'
-        'theme' => 'alliance',
+        'theme' => 'horde',
     ],
     'guild' => [
         'name' => 'My Guild',
