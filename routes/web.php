6e8b2e1c29644651d5beaf1a0940ef49a5929aac
PrivateSniper <privatesniper@gmail.com>
2016-10-11 15:26:57 +0100

    Add character routes.

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,12 @@ Route::get('/home', 'HomeController@index');
 Route::get('/admin', ['as' => 'admin.index', 'uses' => 'AdminController@index']);
 
 /* Raids */
-Route::get('raids',                 [ 'as' => 'raid.index',          'uses' => 'PublicController@raids'        ]);
-Route::get('raid/{id}',             [ 'as' => 'raid.view',           'uses' => 'RaidController@view'           ]);
-Route::post('/raids',               [ 'as' => 'api.post.raid',       'uses' => 'Api\RaidController@store'      ]);
-Route::post('/raids/{id}/fights',   [ 'as' => 'api.post.raid.fight', 'uses' => 'Api\RaidFightController@store' ]);
-Route::get('raid/{rid}/fight/{id}', [ 'as' => 'raid.fight.view',     'uses' => 'RaidFightController@view'      ]);
+Route::get('raids',                  [ 'as' => 'raid.index',          'uses' => 'PublicController@raids'              ]);
+Route::get('raid/{id}',              [ 'as' => 'raid.view',           'uses' => 'RaidController@view'                 ]);
+Route::post('/raids',                [ 'as' => 'api.post.raid',       'uses' => 'Api\RaidController@store'            ]);
+Route::post('/raids/{id}/fights',    [ 'as' => 'api.post.raid.fight', 'uses' => 'Api\RaidFightController@store'       ]);
+Route::get('raid/{rid}/fight/{id}',  [ 'as' => 'raid.fight.view',     'uses' => 'RaidFightController@view'            ]);
+Route::post('/raids/{id}/attendees', [ 'as' => 'api.post.raid.attendee', 'uses' => 'Api\RaidAttendeeController@store' ]);
 
 /* Characters */
 Route::get('characters',   [ 'as' => 'character.index',    'uses' => 'PublicController@characters'   ]);
