3d80f8a4359e9c0db5e6c0fed2fe1dbec0e4d02a
PrivateSniper <privatesniper@gmail.com>
2016-10-11 15:26:25 +0100

    Add RaidAttendee modal.

--- resources/views/raids/view.blade.php
+++ resources/views/raids/view.blade.php
@@ -3,15 +3,18 @@
 @section('content')
 <h1>Raid: {{ $raid->zone->name }}</h1>
 <h2>Date: {{ $raid->date }}</h2>
+@if ($user->can('administrate'))
 @include('raids.fights._createModal')
+@include('raids.attendees._createModal')
+@endif
 
 <div class="row">
     <div class="col-xs-12 col-sm-8">
-        Fights @if ($raid->locked == 0) <button data-toggle="modal" data-target="#createRaidFightModal" class="btn btn-sm btn-primary pull-right">Create Fight</button> @endif
+        Fights @if ($raid->locked == 0 && $user->can('administrate')) <button data-toggle="modal" data-target="#createRaidFightModal" class="btn btn-sm btn-primary pull-right">Create Fight</button> @endif
         @include('raids.fights._list')
     </div>
     <div class="col-xs-12 col-sm-4">
-        Attendees @if ($raid->locked == 0) <button data-toggle="modal" data-target="#createRaidAttendeeModal" class="btn btn-sm btn-success pull-right">Add Attendee</button> @endif
+        Attendees @if ($raid->locked == 0 && $user->can('administrate')) <button data-toggle="modal" data-target="#createRaidAttendeeModal" class="btn btn-sm btn-success pull-right">Add Attendee</button> @endif
     </div>
 </div>
 @endsection
