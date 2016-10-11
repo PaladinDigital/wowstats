3dc58a9b477c94da82d4669699a1ad3e39d350e2
PrivateSniper <privatesniper@gmail.com>
2016-10-11 14:25:46 +0100

    Add button to add raid attendee.

--- resources/views/raids/view.blade.php
+++ resources/views/raids/view.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
 <h1>Raid: {{ $raid->zone->name }}</h1>
-<h2>Date: {{ $raid->date }} @if ($raid->locked == 0) <button data-toggle="modal" data-target="#createRaidFightModal" class="btn btn-primary pull-right">Create Fight</button> @endif</h2>
+<h2>Date: {{ $raid->date }}</h2>
 @include('raids.fights._createModal')
 
 <div class="row">
     <div class="col-xs-12 col-sm-8">
-        Fights
+        Fights @if ($raid->locked == 0) <button data-toggle="modal" data-target="#createRaidFightModal" class="btn btn-sm btn-primary pull-right">Create Fight</button> @endif
         @include('raids.fights._list')
     </div>
     <div class="col-xs-12 col-sm-4">
-        Characters
+        Attendees @if ($raid->locked == 0) <button data-toggle="modal" data-target="#createRaidAttendeeModal" class="btn btn-sm btn-success pull-right">Add Attendee</button> @endif
     </div>
 </div>
 @endsection
