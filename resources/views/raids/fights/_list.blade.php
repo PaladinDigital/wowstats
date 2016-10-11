9b084c806c9f34cd24d20b741622cedb5706ce93
PrivateSniper <privatesniper@gmail.com>
2016-10-11 14:01:05 +0100

    Add fight length to list.

--- resources/views/raids/fights/_list.blade.php
+++ resources/views/raids/fights/_list.blade.php
@@ -1,14 +1,20 @@
 @if (isset($fights) && count($fights) > 0)
     <table class="table">
     <thead>
-        <tr><th>Boss</th><th>Killed</th></tr>
+        <tr>
+            <th>Boss</th>
+            <th>Killed</th>
+            <th>Length</th>
+        </tr>
     </thead>
     <tbody>
     @foreach($fights as $fight)
         <?php
             $killed = $fight->killed;
+            $duration = $fight->length;
+            $duration = gmdate('i:s', $duration);
         ?>
-        <tr><td>{{ $fight->boss->name }}</td><td><?php echo ($killed == 1) ? 'Kill' : 'No' ?></td></tr>
+        <tr><td>{{ $fight->boss->name }}</td><td><?php echo ($killed == 1) ? 'Kill' : 'No' ?></td><td>{{ $duration }}</td></tr>
     @endforeach
     </tbody>
     </table>
