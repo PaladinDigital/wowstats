fd1f18220256c0e0a9f3d34c5801de0ad8b41848
PrivateSniper <privatesniper@gmail.com>
2016-10-11 12:45:05 +0100

    Add fight length to db schema.

---
+++ database/migrations/2016_10_11_114126_add_length_to_raid_fights_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLengthToRaidFightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('raid_fights', function ($table) {
+            $table->time('length')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('raid_fights', function ($table) {
+            $table->dropColumn('length');
+        });
+    }
+}
