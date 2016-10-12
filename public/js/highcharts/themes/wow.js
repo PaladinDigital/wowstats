dbee63be2d1a4d03b42b1ca496dd5d7454f721d3
PrivateSniper <privatesniper@gmail.com>
2016-10-12 15:51:52 +0100

    Push WiP changes.

--- public/js/highcharts/themes/wow.js
+++ public/js/highcharts/themes/wow.js
@@ -24,9 +24,7 @@
 
         'use strict';
         Highcharts.theme = {
-            colors: ['#DDDF0D', '#7798BF', '#55BF3B', '#DF5353', '#aaeeee', '#ff0066', '#eeaaee',
-                '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'
-            ],
+            colors: ['#FFFFFF', '#FF77FF' ],
             chart: {
                 backgroundColor: '#111',
                 borderWidth: 0,
--- resources/views/raids/fights/view.blade.php
+++ resources/views/raids/fights/view.blade.php
@@ -12,6 +12,22 @@
     @foreach($stats as $stat => $data)
         @if ($stat == 'dps')
             <div id="dps_chart"></div>
+            <table id="dps_data">
+                <thead>
+                    <tr>
+                        @foreach($data as $s)
+                        <th>{{ $s['character']->name }}</th>
+                        @endforeach
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        @foreach($data as $s)
+                        <td>{{ $s['value'] }}</td>
+                        @endforeach
+                    </tr>
+                </tbody>
+            </table>
         @else
         @if (count($data) > 0)
             <div class="col-xs-12 col-sm-6 col-md-4">
@@ -35,71 +51,23 @@
 <!-- DPS Chart -->
 <script>
     $(function () {
-    $('#dps_chart').highcharts({
-    chart: {
-    type: 'column'
-    },
-    title: {
-    text: 'Monthly Average Rainfall'
-    },
-    subtitle: {
-    text: 'Source: WorldClimate.com'
-    },
-    xAxis: {
-    categories: [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec'
-    ],
-    crosshair: true
-    },
-    yAxis: {
-    min: 0,
-    title: {
-    text: 'Rainfall (mm)'
-    }
-    },
-    tooltip: {
-    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-        footerFormat: '</table>',
-    shared: true,
-    useHTML: true
-    },
-    plotOptions: {
-    column: {
-    pointPadding: 0.2,
-    borderWidth: 0
-    }
-    },
-    series: [{
-    name: 'Tokyo',
-    data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-
-    }, {
-    name: 'New York',
-    data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-
-    }, {
-    name: 'London',
-    data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-
-    }, {
-    name: 'Berlin',
-    data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
-
-    }]
-    });
+        $('#dps_chart').highcharts({
+            data: {
+                table: 'dps_data'
+            },
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Fight DPS'
+            },
+            yAxis: {
+                allowDecimals: false,
+                title: {
+                    text: 'DPS'
+                }
+            }
+        });
     });
 </script>
 @endsection
