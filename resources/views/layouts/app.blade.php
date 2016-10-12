de78cac256c618581a015083bdf31d203744515e
PrivateSniper <privatesniper@gmail.com>
2016-10-12 15:41:18 +0100

    Add highcharts theme.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,7 @@
     <script src="{{ asset('js/jquery-3.1.1.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/highcharts/highcharts.js') }}"></script>
+    <script src="{{ asset('js/highcharts/themes/wow.js') }}"></script>
     @yield('scripts')
 </body>
 </html>
