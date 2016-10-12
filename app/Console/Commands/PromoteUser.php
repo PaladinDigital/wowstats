62f76bec1fd7b95d5d5c0ac2596dd3e0f6fbd3d6
PrivateSniper <privatesniper@gmail.com>
2016-10-12 10:59:39 +0100

    Create artisan command to promote users to administrators.

---
+++ app/Console/Commands/PromoteUser.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace WoWStats\Console\Commands;
+
+use Illuminate\Console\Command;
+use WoWStats\User;
+
+class PromoteUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'promote:user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Promote a user to administrator.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $users = User::where('admin', 0)->get(['id', 'name', 'email']);
+
+        // List users to select
+        $headers = ['id', 'name', 'email'];
+        $this->table($headers, $users->toArray());
+
+        $valid_ids = [];
+        foreach($users as $u) {
+            $valid_ids[] = $u['id'];
+        }
+
+        // Ask which user to promote
+        $id  = $this->choice('Which user would you like to promote?', $valid_ids, null);
+
+        // Confirm choice
+        $user = User::where('id', $id)->first();
+        if ($this->confirm('Are you sure you wish to promote: ' . $user->name . '?')) {
+            $user->admin = 1;
+            $user->save();
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace WoWStats\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use WoWStats\Console\Commands\PromoteUser;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        PromoteUser::class,
     ];
 
     /**
