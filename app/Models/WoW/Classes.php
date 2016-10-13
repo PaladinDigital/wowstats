b1ec4322ba8e6d42431a2e483532f67fb73ec958
PrivateSniper <privatesniper@gmail.com>
2016-10-13 09:12:06 +0100

    Add rgb values for class colors.

--- app/Models/WoW/Classes.php
+++ app/Models/WoW/Classes.php
@@ -7,18 +7,114 @@ Class Classes
     public function __construct()
     {
         $this->class_data = [
-            1 => ['class' => 'Warrior', 'color' => '#C79C6E'],
-            2 => ['class' => 'Paladin', 'color' => '#F58CBA'],
-            3 => ['class' => 'Hunter', 'color' => '#ABD473'],
-            4 => ['class' => 'Rogue', 'color' => '#FFF569'],
-            5 => ['class' => 'Priest', 'color' => '#FFFFFF'],
-            6 => ['class' => 'Death Knight', 'color' => '#C41F3B'],
-            7 => ['class' => 'Shaman', 'color' => '#0070DE'],
-            8 => ['class' => 'Mage', 'color' => '#69CCF0'],
-            9 => ['class' => 'Warlock', 'color' => '#9482C9'],
-            10 => ['class' => 'Monk', 'color' => '#00FF96'],
-            11 => ['class' => 'Druid', 'color' => '#FF7D0A'],
-            12 => ['class' => 'Demon Hunter', 'color' => '#A330C9']
+            1 => [
+                'class' => 'Warrior',
+                'color' => '#C79C6E',
+                'color_rgb' => [
+                    'r' => 199,
+                    'g' => 156,
+                    'b' => 110
+                ]
+            ],
+            2 => [
+                'class' => 'Paladin',
+                'color' => '#F58CBA',
+                'color_rgb' => [
+                    'r' => 245,
+                    'g' => 140,
+                    'b' => 186
+                ]
+            ],
+            3 => [
+                'class' => 'Hunter',
+                'color' => '#ABD473',
+                'color_rgb' => [
+                    'r' => 171,
+                    'g' => 212,
+                    'b' => 115
+                ]
+            ],
+            4 => [
+                'class' => 'Rogue',
+                'color' => '#FFF569',
+                'color_rgb' => [
+                    'r' => 255,
+                    'g' => 245,
+                    'b' => 105
+                ]
+            ],
+            5 => [
+                'class' => 'Priest',
+                'color' => '#FFFFFF',
+                'color_rgb' => [
+                    'r' => 255,
+                    'g' => 255,
+                    'b' => 255
+                ]
+            ],
+            6 => [
+                'class' => 'Death Knight',
+                'color' => '#C41F3B',
+                'color_rgb' => [
+                    'r' => 196,
+                    'g' => 31,
+                    'b' => 59
+                ]
+            ],
+            7 => [
+                'class' => 'Shaman',
+                'color' => '#0070DE',
+                'color_rgb' => [
+                    'r' => 0,
+                    'g' => 112,
+                    'b' => 222
+                ]
+            ],
+            8 => [
+                'class' => 'Mage',
+                'color' => '#69CCF0',
+                'color_rgb' => [
+                    'r' => 105,
+                    'g' => 204,
+                    'b' => 240
+                ]
+            ],
+            9 => [
+                'class' => 'Warlock',
+                'color' => '#9482C9',
+                'color_rgb' => [
+                    'r' => 148,
+                    'g' => 130,
+                    'b' => 201
+                ]
+            ],
+            10 => [
+                'class' => 'Monk',
+                'color' => '#00FF96',
+                'color_rgb' => [
+                    'r' => 0,
+                    'g' => 255,
+                    'b' => 150
+                ]
+            ],
+            11 => [
+                'class' => 'Druid',
+                'color' => '#FF7D0A',
+                'color_rgb' => [
+                    'r' => 255,
+                    'g' => 125,
+                    'b' => 10
+                ]
+            ],
+            12 => [
+                'class' => 'Demon Hunter',
+                'color' => '#A330C9',
+                'color_rgb' => [
+                    'r' => 163,
+                    'g' => 48,
+                    'b' => 201
+                ]
+            ]
         ];
     }
 
