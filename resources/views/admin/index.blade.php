8b62089ef284ebf8451aec6a48e39ba10b768b40
PrivateSniper <privatesniper@gmail.com>
2016-10-21 17:58:19 +0100

    Add admin index view.

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <section>
+        <h1>Dashboard</h1>
+        @include('admin/widgets/_statsWidget', ['class' => '', 'stat' => 'Users', 'value' => $stats['users']])
+        @include('admin/widgets/_statsWidget', ['class' => '', 'stat' => 'Raids', 'value' => $stats['raids']])
+    </section>
+
+    <section>
+        <h1>Administration</h1>
+        <ul>
+            <li>Raids</li>
+            <li>Raid Zones</li>
+            <li>Bosses</li>
+        </ul>
+    </section>
+</div>
+@stop
