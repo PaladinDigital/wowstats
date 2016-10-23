2e8457f5b5803fe53f431560f8526fbb58df4263
PrivateSniper <privatesniper@gmail.com>
2016-10-23 10:43:20 +0100

    Show difficulty on fight view.

--- resources/views/raids/fights/view.blade.php
+++ resources/views/raids/fights/view.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Raid: {{ $raid->zone->name }}</h1>
+    <h1>Raid: {{ $raid->zone->name }} ({{ $raid->difficulty() }})</h1>
     <h2>Date: {{ $raid->date }}</h2>
     <h3<?php if ($fight->killed === 1) { echo ' class="killed"'; } ?>>Fight: {{ $fight->boss->name }} @if ($fight->killed === 1) (Kill) @endif @if ( isset($user) && $user->can('administrate') )<button data-toggle="modal" data-target="#createCharacterStatsModal" class="btn btn-sm btn-primary pull-right">Add Character Stats</button>@endif</h3>
     @if ( isset($user) && $user->can('administrate') )
