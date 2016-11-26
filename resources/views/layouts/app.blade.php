40a10cde94b62028d7662d88941bfb9e5a507357
PrivateSniper <privatesniper@gmail.com>
2016-11-26 07:58:15 +0000

    Add font-awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" />
     @if (isset($theme))
         @if ($theme == 'horde')
             <link href="{{ asset('css/horde.css') }}" rel="stylesheet" />
