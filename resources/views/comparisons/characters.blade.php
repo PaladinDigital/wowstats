999eda084935dd899d018cd9001c80aae072819f
PrivateSniper <privatesniper@gmail.com>
2016-12-02 09:15:11 +0000

    Fix loading charts when data is not set.

--- resources/views/comparisons/characters.blade.php
+++ resources/views/comparisons/characters.blade.php
@@ -3,16 +3,36 @@
 @section('content')
 @if (isset($hps_comparison))
     <div id="hps_comparison"></div>
-
-    <?php $data = [
-        'container' => 'hps_comparison',
-        'title' => 'HPS Comparison',
-        'categories' => $hps_comparison['categories'],
-        'series' => $hps_comparison['series'],
-    ]; ?>
 @endif
+@if (isset($tank_comparison))
+    <div id="tank_comparison"></div>
+@endif
+@if (isset($dps_comparison))
+    <div id="dps_comparison"></div>
+@endif
+<?php
+$data = [];
+    if (isset($hps_comparison)) {
+        $data['container'] = 'hps_comparison';
+        $data['title'] = 'HPS Comparison';
+        $data['categories'] = $hps_comparison['categories'];
+        $data['series'] = $hps_comparison['series'];
+    } elseif (isset($tank_comparison)) {
+        $data['container'] = 'tank_comparison';
+        $data['title'] = 'DTPS Comparison';
+        $data['categories'] = $dtps_comparison['categories'];
+        $data['series'] = $dtps_comparison['series'];
+    } elseif (isset($dps_comparison)) {
+        $data['container'] = 'dps_comparison';
+        $data['title'] = 'DPS Comparison';
+        $data['categories'] = $dps_comparison['categories'];
+        $data['series'] = $dps_comparison['series'];
+    }
+?>
 @endsection
 
 @section('scripts')
-    @include('highcharts.chart.metric_compare.characters.spline', $data)
+    @if (isset($data['container']))
+        @include('highcharts.chart.metric_compare.characters.spline', $data)
+    @endif
 @endsection
