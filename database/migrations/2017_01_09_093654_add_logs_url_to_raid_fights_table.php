e916ac2fe1f38b363178a9d7b18ca67355883b72
David Craig <david.craig10@nhs.net>
2017-01-09 09:40:13 +0000

    Add logs url field to raid_fights table.

---
+++ database/migrations/2017_01_09_093654_add_logs_url_to_raid_fights_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWlUrlToRaidFightsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('raid_fights', function ($table) {
+            $table->string('logs_url')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('raid_fights', function ($table) {
+            $table->dropColumn('logs_url');
+        });
+    }
+}
