a82e1f4d792b5524ecc3a985772c384b36c8bffb
David Craig <david.craig10@nhs.net>
2017-01-09 09:55:32 +0000

    Fix migration class name.

--- database/migrations/2017_01_09_093654_add_logs_url_to_raid_fights_table.php
+++ database/migrations/2017_01_09_093654_add_logs_url_to_raid_fights_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddWlUrlToRaidFightsTable extends Migration
+class AddLogsUrlToRaidFightsTable extends Migration
 {
     public function up()
     {
