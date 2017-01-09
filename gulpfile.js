f47a3326ad8cd38717e579b7cf6224dbd8d0c56e
PrivateSniper <privatesniper@gmail.com>
2017-01-09 13:39:15 +0000

    Re-add vue components and bootstrap for later vuejs use.

--- gulpfile.js
+++ gulpfile.js
@@ -22,3 +22,20 @@ elixir(mix => {
         'fightlock.js',
     ]);
 });
+
+require('laravel-elixir-vue-2');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application as well as publishing vendor resources.
+ |
+ */
+
+elixir((mix) => {
+    mix.webpack('app.js');
+});
