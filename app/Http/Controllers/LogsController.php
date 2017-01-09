e1fdc7bf01b120f76648d74363db80f3d7dff662
PrivateSniper <privatesniper@gmail.com>
2017-01-09 20:36:19 +0000

    Turn value into a float.

--- app/Http/Controllers/LogsController.php
+++ app/Http/Controllers/LogsController.php
@@ -1,5 +1,6 @@
 <?php namespace WoWStats\Http\Controllers;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use WoWStats\Models\Character;
 use WoWStats\Models\CharacterStats;
@@ -20,6 +21,7 @@ class LogsController extends Controller
         $data['metrics'] = $metrics;
         $data['raid_id'] = $raid_id;
         $data['fight_id'] = $fight_id;
+
         // Log upload entry point, user selects raid, fight, metrics and then clicks upload.
         return view('logs.upload.import', $data);
     }
@@ -62,6 +64,7 @@ class LogsController extends Controller
         }
 
         $csv = $request->file($tag);
+
         return $csv;
     }
 
@@ -70,6 +73,8 @@ class LogsController extends Controller
         $reader = CsvReader::createFromPath($csv->path());
 
         switch ($metric) {
+
+            // All
             case 'deaths':
                 $deaths = $this->buildDeathsArray($reader);
                 // Get the deaths metric id.
@@ -80,14 +85,85 @@ class LogsController extends Controller
                     if (Character::characterExists($character)) {
                         $char = Character::where('name', $character)->firstOrFail();
 
-                        $data = [
-                            'fight_id' => $fight_id,
-                            'character_id' => $char->id,
-                            'metric_id' => $metric_id,
-                            'value' => $death_count,
-                        ];
+                        // Check if the stat already exists
+                        $exists = CharacterStats::exists($fight_id, $char->id, $metric_id);
 
-                        CharacterStats::create($data);
+                        if (!$exists) {
+                            $data = [
+                                'fight_id' => $fight_id,
+                                'character_id' => $char->id,
+                                'metric_id' => $metric_id,
+                                'value' => $death_count,
+                            ];
+
+                            CharacterStats::create($data);
+                        }
+                    }
+                }
+                break;
+
+            // DPS
+            case 'damage':
+            case 'dps':
+                break;
+
+            // Tanking
+            case 'damage_taken':
+            case 'dtps':
+                break;
+
+            // Healing
+            case 'healing':
+            case 'hps':
+                $healingStats = $this->buildHealingArray($reader);
+                // Get the deaths metric id.
+                $healing_id = Metric::where('name', 'healing')->first()->id;
+                $hps_id = Metric::where('name', 'healing')->first()->id;
+
+                $hps = $healingStats['hps'];
+                $healing = $healingStats['healing'];
+
+                // Create the HPS stats.
+                foreach ($hps as $character => $hps_value) {
+                    // Check if the character exists
+                    if (Character::characterExists($character)) {
+                        $char = Character::where('name', $character)->firstOrFail();
+
+                        // Check if the stat already exists
+                        $exists = CharacterStats::exists($fight_id, $char->id, $hps_id);
+
+                        if (!$exists) {
+                            $data = [
+                                'fight_id' => $fight_id,
+                                'character_id' => $char->id,
+                                'metric_id' => $hps_id,
+                                'value' => $hps_value,
+                            ];
+
+                            CharacterStats::create($data);
+                        }
+                    }
+                }
+
+                // Create the Healing stats.
+                foreach ($healing as $character => $healing_done) {
+                    // Check if the character exists
+                    if (Character::characterExists($character)) {
+                        $char = Character::where('name', $character)->firstOrFail();
+
+                        // Check if the stat already exists
+                        $exists = CharacterStats::exists($fight_id, $char->id, $healing_id);
+
+                        if (!$exists) {
+                            $data = [
+                                'fight_id' => $fight_id,
+                                'character_id' => $char->id,
+                                'metric_id' => $healing_id,
+                                'value' => $healing_done,
+                            ];
+
+                            CharacterStats::create($data);
+                        }
                     }
                 }
                 break;
@@ -123,5 +199,114 @@ class LogsController extends Controller
         }
 
         return $deaths;
+    }
+
+    public function buildHealingArray($reader)
+    {
+        $healing = [];
+        $hps = [];
+        foreach ($reader as $index => $row) {
+            // Skip headers
+            if ($index === 0) {
+                continue;
+            }
+
+            // $row[0] = Perf%,
+            // $row[1] = Character
+            // $row[2] = Amount "raw$percent%shortm"
+            // $row[3] = Overheal
+            // $row[4] = iLvl
+            // $row[5] = iLvl %
+            // $row[6] = Active
+            // $row[7] = HPS
+            $character = $row[1];
+
+            $healing_amount = $this->extractWlMetricShortform($row[2]);
+            $hps_amount = $row[7];
+
+            $healing[$character] = $healing_amount;
+            $hps[$character] = $hps_amount;
+        }
+
+        return [
+            'healing' => $healing,
+            'hps' => $hps,
+        ];
+    }
+
+    public function buildDamageArray($reader)
+    {
+        $damage = [];
+        $dps = [];
+        foreach ($reader as $index => $row) {
+            // Skip headers
+            if ($index === 0) {
+                continue;
+            }
+
+            // $row[0] = Perf%,
+            // $row[1] = Character
+            // $row[2] = Amount "raw$percent%shortm"
+            // $row[3] = iLvl
+            // $row[4] = iLvl %
+            // $row[5] = Active
+            // $row[6] = DPS
+            $character = $row[1];
+
+            $damage_amount = $this->extractWlMetricShortform($row[2]);
+            $dps_amount = $row[6];
+
+            $damage[$character] = $damage_amount;
+            $dps[$character] = $dps_amount;
+        }
+
+        return [
+            'damage' => $damage,
+            'dps' => $dps,
+        ];
+    }
+
+    public function buildTankingArray($reader)
+    {
+        $damage_taken = [];
+        $dtps = [];
+        foreach ($reader as $index => $row) {
+            // Skip headers
+            if ($index === 0) {
+                continue;
+            }
+
+            // $row[0] = Character
+            // $row[1] = Amount "raw$percent%shortm"
+            // $row[2] = iLvl
+            // $row[3] = Active
+            // $row[4] = DTPS
+
+            $character = $row[1];
+
+            $damage_taken_amount = $this->extractWlMetricShortform($row[2]);
+            $dtps_amount = $row[4];
+
+            $damage_taken[$character] = $damage_taken_amount;
+            $dtps[$character] = $dtps_amount;
+        }
+
+        return [
+            'damage_taken' => $damage_taken_amount,
+            'dtps' => $dtps,
+        ];
+    }
+
+    public function extractWlMetricShortform($column)
+    {
+        // "raw$percent%shortm"
+
+        $percent_pos = strpos($column, "%");
+        $m_pos = strpos($column, "m");
+        $len = $m_pos - $percent_pos;
+
+        $short = substr($percent_pos, $len);
+        $value = str_replace(',', "", $short);
+        return (float)$value;
     }
 }
