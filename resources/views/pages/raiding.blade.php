0cc768e670fb5615381f85faf2bdd54940bbc009
PrivateSniper <privatesniper@gmail.com>
2017-01-20 14:02:26 +0000

    Add blurb.

--- resources/views/pages/raiding.blade.php
+++ resources/views/pages/raiding.blade.php
@@ -5,6 +5,19 @@
 
     <h2>Progression Team</h2>
     <p>Our progression team trailblaze new content to learn the tactics and get boss kills under out belt prior to opening up the raid to the guild raid team.</p>
+    @if (isset($progression_team) && !empty($progression_team))
+        <h3>Roster</h3>
+        @foreach($progression_team as $character)
+
+        @endforeach
+    @endif
 
     <h2>Guild Raid Team</h2>
+    <p>The guild raid team raids regularly as well, consists of a large amount of our progression team as well as guild members who want to raid and trial for the progression team.</p>
+    @if (isset($raid_team) && !empty($raid_team))
+        <h3>Roster</h3>
+        @foreach($raid_team as $character)
+
+        @endforeach
+    @endif
 @endsection
