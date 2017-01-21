97de4f876462489695feeadb2bc91ccc57921083
PrivateSniper <privatesniper@gmail.com>
2017-01-21 14:12:10 +0000

    Add raiding and officers to logged in navbar.

--- resources/views/layouts/_navbar.blade.php
+++ resources/views/layouts/_navbar.blade.php
@@ -19,6 +19,8 @@
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
+                <li><a href="{{ url('/raiding') }}">Raiding</a></li>
+                <li><a href="{{ url('/officers') }}">Officers</a></li>
                 <li><a href="{{ url('/raids') }}">Raids</a></li>
                 <li><a href="{{ url('/characters') }}">Characters</a></li>
                 <li><a href="{{ url('/leaderboards') }}">Leaderboards</a></li>
