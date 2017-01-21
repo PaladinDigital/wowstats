40c2048beb93b3d7b9b23beb6a78b06d1f9d07f7
PrivateSniper <privatesniper@gmail.com>
2017-01-21 14:52:42 +0000

    Update progression team syntax.

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,34 +1,38 @@
 <?php namespace WoWStats\Http\Controllers;
 
-use WoWStats\Models\Raid;
-use WoWStats\Models\RaidBoss;
-use WoWStats\Models\RaidFight;
 use WoWStats\Models\Character;
-use WoWStats\Models\RaidAttendee;
 
 class PageController extends Controller
 {
+    public $characters;
+
+    public function __construct()
+    {
+        $healers = [
+            'Sniperdrood',
+            'Zonraja', 'Locutor', 'Pepperstash'
+        ];
+        $tanks = [ 'Nuuruhuine', 'Wulfar' ];
+        $dps = [
+            'Murmundamus', 'Wolirraw',
+            'Zenjaquin', 'Vagrasis', 'Labobbob', 'Phanotos', 'Bananael',
+            'Glasha', 'Azkadélia',
+        ];
+
+        $this->characters = [
+            'tanks'   => Character::whereIn('name', $tanks)->get(),
+            'healers' => Character::whereIn('name', $healers)->get(),
+            'dps'     => Character::whereIn('name', $dps)->get(),
+        ];
+    }
+
     public function raiding()
     {
         $data = [
             'progression_team' => [
                 /* Officers */
-                (object)[ 'name' => 'Wolirraw',    'class' => 'Hunter',       'cssClass'  => 'hunter'       ],
-                (object)[ 'name' => 'Murmundamus', 'class' => 'Warlock',      'cssClass'  => 'warlock'      ],
-                (object)[ 'name' => 'Nuuruhuine',  'class' => 'Death Knight', 'cssClass'  => 'death_knight' ],
-                (object)[ 'name' => 'Sniperdrood', 'class' => 'Druid',        'cssClass'  => 'druid'        ],
-                (object)[ 'name' => 'Labobbob',    'class' => 'Death Knight', 'cassClass' => 'death_knight' ],
-                (object)[ 'name' => 'Wulfar',      'class' => 'Druid',        'cssClass'  => 'druid'        ],
-                /* DPS */
-                (object)[ 'name' => 'Vagrasis',    'class' => 'Mage',         'cssClass'  => 'mage'         ],
-                (object)[ 'name' => 'Phanotos',    'class' => 'Rogue',        'cssClass'  => 'rogue'        ],
-                (object)[ 'name' => 'Bananael',    'class' => 'Demon Hunter', 'cssClass'  => 'demon_hunter' ],
-                (object)[ 'name' => 'Glasha',      'class' => 'Warrior',      'cssClass'  => 'warrior'      ],
-                (object)[ 'name' => 'Zenjaquin',   'class' => 'Hunter',       'cssClass'  => 'hunter'       ],
-                /* Healers */
-                (object)[ 'name' => 'Locutor',     'class' => 'Priest',       'cssClass'  => 'priest'       ],
-                (object)[ 'name' => 'Pepperstash', 'class' => 'monk',         'cssClass'  => 'monk'         ],
-                (object)[ 'name' => 'Zonraja',     'class' => 'druid',        'cssClass'  => 'druid'        ],
+                'tanks' => $this->characters->tanks,
+                'healers' => $this->characters->healers,
             ],
             'raid_team' => [
                 (object)[ 'name' => 'Azkadélia', 'class' => 'Mage', 'cssClass' => 'mage' ],
--- resources/views/pages/raiding.blade.php
+++ resources/views/pages/raiding.blade.php
@@ -7,11 +7,16 @@
     <p>Our progression team trailblaze new content to learn the tactics and get boss kills under out belt prior to opening up the raid to the guild raid team.</p>
     @if (isset($progression_team) && !empty($progression_team))
         <h3>Roster</h3>
-        <ul>
-        @foreach($progression_team as $character)
-            <li>{{ $character->name }}</li>
+
+        @foreach($progression_team as $role => $characters)
+            <h4>{{ $role }}</h4>
+            <ul>
+                @foreach ($characters as $c)
+                    <li class="{{ $c->cssClass() }}">{{ $c->name }}</li>
+                @endforeach
+            </ul>
         @endforeach
-        </ul>
+        <ul>
     @endif
 
     <h2>Guild Raid Team</h2>
