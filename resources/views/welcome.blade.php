cbca60adbb10629d42586ff65190cfd7d4efe573
PrivateSniper <privatesniper@gmail.com>
2017-01-21 14:20:04 +0000

    Add server/region.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,7 +77,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <p>{{ $guildName }}</p>
+                    <p>{{ $guildName }}<br/><small>Magtheridon EU</small></p>
                 </div>
                 <p>A guild for those who like to raid but aren’t interested in elitist dongsnoffler BS. We raid Seriously but Casually meaning we expect our members to take it seriously but we aren’t screaming at eachother and guild kicking for Morging out.</p>
                 <p>We keep the membership at 18+ due to the generally adult nature of the humour. Do not join us if you are easily offended but if you like downing current content and dick and fart jokes, this could be the place for you.</p>
