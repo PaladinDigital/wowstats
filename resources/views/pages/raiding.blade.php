70db78a0ec21c07eb9e2096027cff71b955fb6dc
PrivateSniper <privatesniper@gmail.com>
2017-01-27 20:27:00 +0000

    Update raiding page order.

--- resources/views/pages/raiding.blade.php
+++ resources/views/pages/raiding.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+    <h1>Raiding Rules</h1>
+    <ul>
+        <li>All Raid Start Times are in Server Time.</li>
+        <li>Raiders must use the appropriate flasks/food/runes etc.</li>
+        <li>Raids last until at least 23:00 server time unless otherwise stated. We expect all members of the progression team to stay until the end unless the officers are made aware beforehand.</li>
+        <li>Currently the officers are running the raids, listen to what they are telling you. If you think you could be a raid leader then speak to the officers about it.</li>
+    </ul>
+
+    <h2>How To Join</h2>
+    If you would like to join the progression team there are additional requirements in order to ensure we are not carrying members through content.
+    <ul>
+        <li>ALL: Ability to follow tactics. (Assessed by committee decision by the officers).</li>
+        <li>ALL: Ability to remain focused during the fights (ie not tunnel visioning and standing in shit).</li>
+        <li>ALL: No pugging content prior to running it with the guild.</li>
+        <li>DPS: Minimum of 250k DPS, this will be assessed on guild raid runs (Subject to other factors such as moving out of shit etc).</li>
+        <li>Healers: Dispelling appropriately when required.  Reducing overhealing where possible.</li>
+    </ul>
+
     <h1>Raiding Teams</h1>
 
     <h2>Progression Team</h2>
@@ -31,22 +49,4 @@
             </ul>
         @endforeach
     @endif
-
-    <h2>Raiding Rules</h2>
-    <ul>
-        <li>All Raid Start Times are in Server Time.</li>
-        <li>Raiders must use the appropriate flasks/food/runes etc.</li>
-        <li>Raids last until at least 23:00 server time unless otherwise stated. We expect all members of the progression team to stay until the end unless the officers are made aware beforehand.</li>
-        <li>Currently the officers are running the raids, listen to what they are telling you. If you think you could be a raid leader then speak to the officers about it.</li>
-    </ul>
-
-    <h2>How To Join</h2>
-    If you would like to join the progression team there are additional requirements in order to ensure we are not carrying members through content.
-    <ul>
-        <li>ALL: Ability to follow tactics. (Assessed by committee decision by the officers).</li>
-        <li>ALL: Ability to remain focused during the fights (ie not tunnel visioning and standing in shit).</li>
-        <li>ALL: No pugging content prior to running it with the guild.</li>
-        <li>DPS: Minimum of 250k DPS, this will be assessed on guild raid runs (Subject to other factors such as moving out of shit etc).</li>
-        <li>Healers: Dispelling appropriately when required.  Reducing overhealing where possible.</li>
-    </ul>
 @endsection
