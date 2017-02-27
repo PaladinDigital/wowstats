bbbf4cac990ac92e7179f0f769ef2134b985e345
PrivateSniper <privatesniper@gmail.com>
2017-02-27 12:22:12 +0000

    Add officer images.

--- resources/views/pages/officers.blade.php
+++ resources/views/pages/officers.blade.php
@@ -5,6 +5,7 @@
 
     @foreach($officers as $officer)
         <div class="officer">
+            <img src="{{ asset('images/characters/' . $officer->name . '.jpg') }}" width="370">
             <h2 class="{{ $officer->cssClass }}">{{ $officer->name }} <small>Rank: {{ $officer->rank or '' }}</small></h2>
             <p>{{ $officer->description }}</p>
         </div>
