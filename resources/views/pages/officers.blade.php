123d680f15acb2695e05a0bf73cddb1ab5738d13
PrivateSniper <privatesniper@gmail.com>
2017-02-27 12:42:41 +0000

    Update officers page.

--- resources/views/pages/officers.blade.php
+++ resources/views/pages/officers.blade.php
@@ -3,11 +3,19 @@
 @section('content')
     <h1>Officers</h1>
 
+    <div class="row">
     @foreach($officers as $officer)
-        <div class="officer">
-            <img src="{{ asset('images/characters/' . $officer->name . '.jpg') }}" width="370">
-            <h2 class="{{ $officer->cssClass }}">{{ $officer->name }} <small>Rank: {{ $officer->rank or '' }}</small></h2>
-            <p>{{ $officer->description }}</p>
+        <div class="col-xs-12 col-sm-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title {{ $officer->cssClass }}">{{ $officer->name }} <small>Rank: {{ $officer->rank or '' }}</small></h2>
+                </div>
+                <div class="panel-body">
+                    <img class="img-responsive" src="{{ asset('images/characters/' . $officer->name . '.jpg') }}">
+                    <p>{{ $officer->description }}</p>
+                </div>
+            </div>
         </div>
     @endforeach
+    </div>
 @endsection
