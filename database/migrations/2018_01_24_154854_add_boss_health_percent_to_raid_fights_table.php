b741719f27e5d5e7cd3245281381d7f67ea22f72
davidcraig <d.craig@redu.co.uk>
2018-01-24 15:50:56 +0000

    Add column for boss health percentage.

---
+++ database/migrations/2018_01_24_154854_add_boss_health_percent_to_raid_fights_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBossHealthPercentToRaidFightsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('raid_fights', function ($table) {
+            $table->float('boss_health', 3, 2)->default(0);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('raid_fights', function ($table) {
+            $table->dropColumn('boss_health');
+        });
+    }
+}
