9da1c391d613f72f1e57a64fafbae05651c28025
davidcraig <davidcraig1986@gmail.com>
2018-01-27 20:54:14 +0000

    Extract recent stats widget.

--- resources/views/characters/view.blade.php
+++ resources/views/characters/view.blade.php
@@ -32,28 +32,7 @@ if ($isDpsOnly && $mainSpec === 'Unknown') {
                 @endslot
             @endcomponent
 
-                @component('bootstrap4.components._panel')
-                    @slot('header')
-                        Stats: Recent Fights (Last 10 Fights)
-                    @endslot
-                    <ul class="list-group list-group-flush">
-                        @if ($recentStats['deaths'] > 0)
-                            <li class="list-group-item">Deaths: {{ $recentStats['deaths'] }}</li>
-                        @endif
-                        @if (array_key_exists('average_hps', $recentStats))
-                            <li class="list-group-item">Average HPS: {{ $recentStats['average_hps'] }}</li>
-                        @endif
-                        @if (array_key_exists('max_hps', $recentStats))
-                            <li class="list-group-item">Max HPS: {{ $recentStats['max_hps'] }}</li>
-                        @endif
-                        @if (array_key_exists('average_dps', $recentStats))
-                            <li class="list-group-item">Average DPS: {{ $recentStats['average_dps'] }}</li>
-                        @endif
-                        @if (array_key_exists('max_dps', $recentStats))
-                            <li class="list-group-item">Max DPS: {{ $recentStats['max_dps'] }}</li>
-                        @endif
-                    </ul>
-                @endcomponent
+            @include('characters.widgets._recent-stats')
         </div>
     </div>
 
