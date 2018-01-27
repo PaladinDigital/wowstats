354d57c05fd9e1b43e10d317a7b1323dd0e3b6e1
davidcraig <davidcraig1986@gmail.com>
2018-01-27 21:50:11 +0000

    Adjust fight view (Remove visual clutter).

--- resources/views/raids/fights/view.blade.php
+++ resources/views/raids/fights/view.blade.php
@@ -1,44 +1,58 @@
 @extends($layout)
 
 @section('content')
-    <h1>Raid: {{ $raid->zone->name }} ({{ $raid->difficulty() }}) @include('raids.fights.view._lock')</h1>
-    <h2>Date: {{ $raid->date }}</h2>
-    <h3<?php if ($fight->killed === 1) { echo ' class="killed"'; } ?>>Fight: {{ $fight->boss->name }} @if ($fight->killed === 1) (Kill) @endif @include('raids.fights.view.importStats._button') @include('raids.fights.view.addCharacterStats._button')</h3>
-    @if ( isset($user) && $user->can('administrate') && ($fight->locked === 0) )
-        @include('raids.fights.view.addCharacterStats._modal')
-    @endif
-    @if($fight->logs_url !== '')<small><a href="{{ $fight->logs_url  }}" target="_blank" rel="nofollow noreferrer">{{ $fight->logs_url  }}</a></small>@endif
 
-    <?php /* Primary Charts */ ?>
     <div class="row">
-        <div class="col-xs-12 col-md-6">
-            <div id="hps_chart"></div>
+        <div class="col-xs-6 col-md-9">
+            <h1>Raid: {{ $raid->zone->name }} ({{ $raid->difficulty() }})</h1>
+            <h2>Date: {{ $raid->date }}</h2>
+            <h3<?php if ($fight->killed === 1) { echo ' class="killed"'; } ?>>Fight: {{ $fight->boss->name }} @if ($fight->killed === 1) (Kill) @endif</h3>
         </div>
+        <div class="col-xs-6 col-md-3">
+            @if (isset($user) && $user->isAdmin())
+            @component('bootstrap4.components._panel')
+                @slot('header')
+                    Admin
+                @endslot
+                @slot('body')
 
-        <div class="col-xs-12 col-md-6">
-            <div id="dtps_chart"></div>
+                    @can('administrate')
+                        @include('raids.fights.view._lock')
+
+                        @if($fight->locked === 0)
+                            @include('raids.fights.view.importStats._button')
+                            @include('raids.fights.view.addCharacterStats._button')
+                            @include('raids.fights.view.addCharacterStats._modal')
+                        @endif
+                    @endcan
+                @endslot
+            @endcomponent
+            @endif
         </div>
     </div>
 
+
+
+
+
+    <?php /* Primary Charts */ ?>
+
     <div class="row">
-        <div class="col-xs-12">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div id="dps_chart"></div>
         </div>
     </div>
 
-    <?php /* Secondary Charts */ ?>
     <div class="row">
-        <div class="col-xs-12 col-md-4">
-            <div id="healing_chart"></div>
+
+        <div class="col-xs-12 col-md-7">
+            <div id="hps_chart"></div>
         </div>
 
-        <div class="col-xs-12 col-md-4">
-            <div id="damage_chart"></div>
-        </div>
-
-        <div class="col-xs-4">
+        <div class="col-xs-12 col-md-5">
             <div id="damage_taken_chart"></div>
         </div>
+
     </div>
 
     <?php /* Stats */ ?>
@@ -69,7 +83,5 @@
 @include('raids.fights.charts._dtps')
 @include('raids.fights.charts._dps')
 
-@include('raids.fights.charts._healing')
 @include('raids.fights.charts._damage_taken')
-@include('raids.fights.charts._damage')
 @endsection
