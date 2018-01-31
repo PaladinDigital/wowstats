9e7d1df8abd3048a44508459a868f97eec0b6f11
davidcraig <d.craig@redu.co.uk>
2018-01-31 16:27:45 +0000

    Add config required for blog.

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,6 +54,12 @@ return [
             'visibility' => 'public',
         ],
 
+        'blog' => [
+            'driver' => 'local',
+            'root' => public_path('images/blog'),
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => 'your-key',
