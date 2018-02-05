b168e17933c3ed4c4c6dbc3434eb6efe9c786247
davidcraig <d.craig@redu.co.uk>
2018-02-05 15:04:49 +0000

    Reduce size of right hand top panel by adding room inbetween left and right.

--- resources/views/characters/view.blade.php
+++ resources/views/characters/view.blade.php
@@ -13,7 +13,7 @@ if ($isDpsOnly && $mainSpec === 'Unknown') {
 ?>
 
     <div class="row">
-        <div class="col-xs-12 col-md-6">
+        <div class="col-xs-12 col-md-4">
             <h1 class="{{ $cssClass }}">{{ $character->name }}</h1>
             <ul class="list-group">
                 <li class="list-group-item">Main Spec: {{ $mainSpec }}</li>
@@ -22,7 +22,10 @@ if ($isDpsOnly && $mainSpec === 'Unknown') {
                 @endif
             </ul>
         </div>
-        <div class="col-xs-12 col-md-6">
+        <div class="col-xs-12 col-md-4">
+
+        </div>
+        <div class="col-xs-12 col-md-4">
             @if (!$character->isOwned() || $character->isUsers())
             @component('bootstrap4.components._panel')
                 @slot('header')
