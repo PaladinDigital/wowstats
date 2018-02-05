e5f15e632f3d678f7954f5e1afdc85a5cbb41064
davidcraig <d.craig@redu.co.uk>
2018-02-05 15:39:50 +0000

    Pass the user to the welcome controller.

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,7 +11,7 @@ class WelcomeController extends Controller
      */
     public function index()
     {
-        $data = $this->getData();
-        return view('welcome', $data);
+        $user = $this->getUser();
+        return view('welcome', $user);
     }
 }
