77f738602d75e4eb51fa4a3eace4c9e9d7c48877
davidcraig <d.craig@redu.co.uk>
2018-02-12 14:38:19 +0000

    Structure data for counting down.

--- resources/assets/js/components/Countdown.vue
+++ resources/assets/js/components/Countdown.vue
@@ -1,6 +1,5 @@
 <template>
-    <div>
-        {{days}} {{hours}}
+    <div class="countdown">
     </div>
 </template>
 
@@ -20,7 +19,9 @@
     },
     computed: {
       timer() {
-        return moment(this.date, "YYYY-MM-DD HH:mm")
+        let m = moment(this.date, "YYYY-MM-DD HH:mm");
+        window.console.log(moment);
+        return m;
       },
       difference() {
         return this.timer.diff(this.now);
@@ -29,15 +30,55 @@
         return this.timer.diff(this.now, 'years');
       },
       months() {
-        return this.timer.diff(this.now, 'months');
+        const y = this.years;
+        let months = this.timer.diff(this.now, 'months');
+        if (y > 0 && months > 12) {
+          let Ym = y * 12;
+          months = months - Ym;
+        }
+        return months;
       },
       days() {
         const m = this.months;
         let days = this.timer.diff(this.now, 'days');
-        if (m > 1) {
+
+        if (m >= 1) {
+          let fromMonths = this.now.clone();
+          fromMonths.add(m, 'months');
+          return this.timer.diff(fromMonths, 'days');
         }
         return days;
       },
+      hours() {
+        // Remove: days, months, years
+        let fromDate = this.now.clone();
+        fromDate.add(this.years, 'years');
+        fromDate.add(this.months, 'months');
+        fromDate.add(this.days, 'days');
+
+        return this.timer.diff(fromDate, 'hours');
+      },
+      minutes() {
+        // Remove rounded values.
+        let fromDate = this.now.clone();
+        fromDate.add(this.years, 'years');
+        fromDate.add(this.months, 'months');
+        fromDate.add(this.days, 'days');
+        fromDate.add(this.hours, 'hours');
+
+        return this.timer.diff(fromDate, 'minutes');
+      },
+      seconds() {
+        // Remove rounded values.
+        let fromDate = this.now.clone();
+        fromDate.add(this.years, 'years');
+        fromDate.add(this.months, 'months');
+        fromDate.add(this.days, 'days');
+        fromDate.add(this.hours, 'hours');
+        fromDate.add(this.minutes, 'minutes');
+
+        return this.timer.diff(fromDate, 'seconds');
+      }
     },
     mounted() {
       window.setInterval(() => {
@@ -48,5 +89,12 @@
 </script>
 
 <style scoped>
-
+    .countdown {
+        display: flex;
+        flex-direction: row;
+    }
+    .timer {
+        display: flex;
+        flex-direction: column;
+    }
 </style>
